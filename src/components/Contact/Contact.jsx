25ee47aead82df8fe56478b9d900c952bcf5e132
dineshpatel21 <92016407+dineshpatel21@users.noreply.github.com>
2024-05-24 17:58:06 +0530

    Contact section made

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,30 +1,29 @@
 import React from "react";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <footer>
-      <div>
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
         <h2>Contact</h2>
         <p>Fill free to reach out.</p>
       </div>
-      <ul>
-        <li>
+      <ul className={styles.links}>
+        <li className={styles.link}>
           <img src="" alt="Email icon" />
           <a href="mailto:dinesh.patel36936@gmail.com">
             dinesh.patel36936@gmail.com
           </a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src="" alt="Github icon" />
-          <a href="https://github.com/dineshpatel21">
-            Github
+          <a href="https://www.linkedin.com/in/dinesh-patel-b4aa20220?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+            Linkedin.com
           </a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src="" alt="Email icon" />
-          <a href="mailto:dinesh.patel36936@gmail.com">
-            dinesh.patel36936@gmail.com
-          </a>
+          <a href="https://github.com/dineshpatel21">Github.com</a>
         </li>
       </ul>
     </footer>
